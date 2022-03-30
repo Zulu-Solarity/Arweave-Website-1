8e1c7dec5173090ff424ec978d9e34a477f9efba
Zulu-Solarity <102641117+Zulu-Solarity@users.noreply.github.com>
2022-03-30 08:05:45 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Liquid Gold
         </h1>
 
         <p className={styles.description}>
@@ -58,7 +58,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Created by {' '} <a href="https://nextjs.org">Solarity</a>
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
